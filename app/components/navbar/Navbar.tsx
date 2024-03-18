8148c7e7d5e1757ce5e90373a9a7c882cc52aabd
matti909 <matias.saantiago@gmail.com>
2024-03-18 12:26:29 -0300

    .

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,28 +1,24 @@
-'use client'
+"use client";
 
 import React, { useState } from "react";
-import { BiMenuAltRight } from "react-icons/bi";
 import { AiOutlineCloseSquare } from "react-icons/ai";
-import './Navbar.scss'
+import { BiMenuAltRight } from "react-icons/bi";
+import "./Navbar.scss";
 
 interface NavigationProps {
   sections: { id: string; label: string }[];
   visibleSection: string;
-  handleSmoothScroll: (id: string) => void;
-  headerRef: React.RefObject<HTMLElement>;
 }
 
 const Navbar: React.FC<NavigationProps> = ({
   sections,
-  visibleSection,
-  handleSmoothScroll,
-  headerRef,
+  visibleSection
 }) => {
   const [menuOpen, setMenuOpen] = useState(false);
   const menuToggler = () => setMenuOpen((p) => !p);
 
   return (
-    <header ref={headerRef} className="header">
+    <header className={`header ${visibleSection !== "home" ? "bg-white" : "bg-black"}`}>
       <div className="header__content">
         <nav className={`nav ${menuOpen ? "nav--open" : ""}`}>
           <ul className="nav__menu">
@@ -33,18 +29,16 @@ const Navbar: React.FC<NavigationProps> = ({
               >
                 <a
                   href={`#${section.id}`}
-                  onClick={() => handleSmoothScroll(section.id)}
                 >
                   {section.label}
                 </a>
               </li>
             ))}
           </ul>
-
         </nav>
-          <button className="header__toggle" onClick={menuToggler}>
-            {!menuOpen ? <BiMenuAltRight /> : <AiOutlineCloseSquare />}
-          </button>
+        <button className="header__toggle" onClick={menuToggler}>
+          {!menuOpen ? <BiMenuAltRight /> : <AiOutlineCloseSquare />}
+        </button>
       </div>
     </header>
   );
--- app/hooks/useIntersectionObserver.ts
+++ app/hooks/useIntersectionObserver.ts
@@ -1,58 +1,43 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-function useIntersectionObserver(options = {}) {
+interface IntersectionObserverOptions {
+  root?: Element | null;
+  rootMargin?: string;
+  threshold?: number | number[];
+}
+
+function useIntersectionObserver(options: IntersectionObserverOptions = {}) {
   const [visibleSection, setVisibleSection] = useState("home");
-  const headerRef = useRef<HTMLHeadElement>(null);
   const sectionsRef = useRef<HTMLElement[]>([]);
 
   useEffect(() => {
-    const element = sectionsRef.current;
-
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           setVisibleSection(entry.target.getAttribute("id") || "");
-
-          entry.target.id !== "home"
-            ? headerRef.current?.classList.add("bg-white")
-            : headerRef.current?.classList.remove("bg-white");
         }
       });
     }, options);
 
-    //const sections = document.querySelectorAll("section[id]");
-    if (element) {
-      element.forEach((section) => {
-        observer.observe(section);
-      });
-    }
+    const elements = sectionsRef.current;
+    elements.forEach((element) => {
+      observer.observe(element);
+    });
 
     return () => {
-      observer.disconnect();
-    };
-  }, []);
-
-  const handleSmoothScroll = (id: string) => {
-    const targetElement = document.getElementById(id);
-
-    if (targetElement) {
-      const offsetTop = targetElement.offsetTop;
-      const scrollOffset = 60;
-
-      window.scrollTo({
-        top: offsetTop - scrollOffset,
-        behavior: "smooth",
+      elements.forEach((element) => {
+        observer.unobserve(element);
       });
-    }
-  };
+    };
+  }, [options]); // Agrega las opciones como dependencia para actualizar el observer si cambian
 
-  const refCallback = useCallback((element: HTMLElement | null) => {
+  const refCallback = (element: HTMLElement | null) => {
     if (element) {
       sectionsRef.current.push(element);
     }
-  }, []);
+  };
 
-  return { visibleSection, headerRef, handleSmoothScroll, refCallback };
+  return { visibleSection, refCallback };
 }
 
 export default useIntersectionObserver;
